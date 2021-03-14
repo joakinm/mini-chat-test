71d703d69ab439b9a764aa2dd38c206f87560dcf
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-14 15:31:01 +0100

    making main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
