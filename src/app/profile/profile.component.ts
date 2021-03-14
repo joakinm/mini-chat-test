ef73b88a61499198267f0503f04f7b85dbc0f2dc
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-14 16:29:55 +0100

    creating profile view

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  public buttonClicked = false;
+  public user = {
+    name: 'Sophie',
+    imageUrl: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    state: true,
+    location: 'New York',
+    age: 23,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onClick() {
+    this.buttonClicked = !this.buttonClicked;
+  }
 }
