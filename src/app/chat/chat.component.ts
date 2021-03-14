c7e53a47e43262dd75fe2d5266ef27811471db7d
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-14 17:31:14 +0100

    reusable styles to main styles, implementing chat header

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppDataService } from '../app-data.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
-
+  public user: User;
+  constructor(private router: Router, private appDataService: AppDataService) { 
+    this.user = this.appDataService.user;
+  }
+  
   ngOnInit(): void {
   }
 
+  public onReturn() {
+    this.router.navigate(['/profile']);
+  }
 }
