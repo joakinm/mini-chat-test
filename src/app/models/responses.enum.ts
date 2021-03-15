2342388aa3bab88e2f67fa691950e0d291b3c8f2
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-15 11:36:02 +0100

    chat and responses created

---
+++ src/app/models/responses.enum.ts
@@ -0,0 +1,12 @@
+export enum Responses {
+    'At risus viverra adipiscing at in tellus integer feugiat. Id interdum velit laoreet id.' = 0,
+    'Sem integer vitae justo eget magna fermentum iaculis eu.' = 1,
+    'Tincidunt augue interdum velit euismod in pellentesque massa placerat duis.' = 2,
+    'Massa tempor nec feugiat nisl pretium fusce.' = 3,
+    'Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.' = 4,
+    'Id semper risus in hendrerit gravida rutrum quisque non tellus.' = 5,
+    'Urna nunc id cursus metus aliquam eleifend mi. Ipsum a arcu cursus vitae.' = 6,
+    'A condimentum vitae sapien pellentesque habitant. Elementum eu facilisis sed odio morbi quis commodo.' = 7,
+    'Quis imperdiet massa tincidunt nunc pulvinar sapien. Risus sed vulputate odio ut.' = 8,
+    'Quis ipsum suspendisse ultrices gravida dictum. Ante metus dictum at tempor commodo ullamcorper.' = 9
+  }
